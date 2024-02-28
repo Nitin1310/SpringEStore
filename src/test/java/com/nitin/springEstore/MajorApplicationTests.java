2380f4f163889893980c18de59055fb3f5b1bb3d
Nitin1310 <nitin.bhopatkar@gmail.com>
2024-02-28 22:56:35 +0530

    first commit of code

---
+++ src/test/java/com/nitin/springEstore/MajorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitin.springEstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
